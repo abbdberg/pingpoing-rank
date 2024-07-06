59e9f015064c75b9cbaf42c3f6c48b77ae3476cc
Håkon N Berg <hakon.n.berg@gmail.com>
2024-07-06 19:13:09 +0200

    Update TopBar.tsx

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,12 +1,1 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
-
-export const TopBar = () => <AppBar position="static">
-<Toolbar>
-  {/* <IconButton edge="start" color="inherit" aria-label="menu">
-    <MenuIcon />
-  </IconButton> */}
-  <Typography variant="body1" color="inherit">
-    Be the ping to my pong
-  </Typography>
-</Toolbar>
-</AppBar>
+export const TopBar = () => <></>
